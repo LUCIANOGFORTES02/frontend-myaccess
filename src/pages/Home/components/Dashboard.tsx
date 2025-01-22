79a5b8ec80c53c29e653f8886ad766123bcbfd64
Antonio Lisbôa <antonion.2048@gmail.com>
2025-01-22 09:11:08 -0300

    feat: add integration on media counter

--- src/pages/Home/components/Dashboard.tsx
+++ src/pages/Home/components/Dashboard.tsx
@@ -1,19 +1,18 @@
-import { FileImage,FileVideo,FileAudio    } from 'lucide-react';
+import { FileImage,FileVideo, FileAudio } from 'lucide-react';
+import { useContext } from 'react'
+import { AuthContext } from '@/auth/AuthContext';
 import Stat from './Stat';
 
 export  function Dashboard() {
+  const { user } = useContext(AuthContext);
+
   return (
-    
         <div className="flex flex-1 flex-col gap-4 py-8 px-4 ">
           <div className="flex flex-wrap justify-between"> 
-            <Stat title="Imagens" value={5} icon={FileImage }/>
-            <Stat title="Vídeos" value={6} icon={FileVideo }/>
-            <Stat title="Áudios" value={3} icon={FileAudio }/>
+            <Stat title="Imagens" value={user?.media.image || 0} icon={FileImage }/>
+            <Stat title="Vídeos" value={user?.media.video || 0} icon={FileVideo }/>
+            <Stat title="Áudios" value={user?.media.audio || 0} icon={FileAudio }/>
           </div>
         </div>
-
-
-   
-  
   )
 }
